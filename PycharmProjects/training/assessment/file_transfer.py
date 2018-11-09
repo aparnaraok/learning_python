89d7178ddad5fd1e8505ddc4d051143456e718eb
aparnarao <aparnaraok94@gmail.com>
2018-11-09 16:27:44 +0530

    latest commits

--- PycharmProjects/training/assessment/file_transfer.py
+++ PycharmProjects/training/assessment/file_transfer.py
@@ -10,24 +10,12 @@ class FileTransfer(object):
         print ("Initializing print...")
         logging.info("Initializing...")
 
-    def connect_to_server(self):
-        '''Connects to server'''
+        LOG_FILE = 'logging_example.out'
+        logging.basicConfig(
+            filename = LOG_FILE
+            level = logging.DEBUG
 
-        '''
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(("", 9001))  # bind the socket to some port 9001
-        s.listen(5)  # size of the queue is 5
-        # s.settimeout(5)
-        while True:
-            print("Hello ...entered the loop")
-            print(s.accept.__doc__)
-
-            c, a = s.accept()
-            # print (s.accept.__doc__)
-            print("Received connection from  :  :", a)
-            c.send(b"Hello " + bytes(a[0], 'utf-8'))
-            c.close()
-        '''
+        )
     def scan_content(self):
         '''Returns THE content list'''
 
@@ -36,22 +24,16 @@ class FileTransfer(object):
         the_list = ['The', 'the']
         for line in content_list:
             content = line.strip()
-            #print(content)
             for the_line in the_list:
                 if the_line in content:
                     the_content_list.append(content)
-            #the_content_list = [content if the_line in content (for the_line in the_list]
-        #print(the_content_list)
         return the_content_list
     def get_content_list(self):
         '''Scans the README file'''
         with open('README.txt') as fhandle:
             content_list = fhandle.readlines()
-        #print("content list >>>>>", content_list)
 
-        #for line in content_list:
         content_list = [line.strip() for line in content_list]
-        #print ("Printing content.....")
 
         return content_list
 
@@ -80,10 +62,7 @@ class FileTransfer(object):
 
     def main(self):
         self.download_read_me_file()
-        self.connect_to_server()
 
 
 file_obj = FileTransfer()
-#print (file_obj)
-#file_obj.download_read_me_file()
 file_obj.main()
